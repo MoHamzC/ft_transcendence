576dd7aaea9984d215a1c3bfbd8807b5ed91136f
marionelstrt <mariontarento@gmail.com>
2025-07-30 23:42:26 +0200

    feat: 42 auth button

--- marion/website/src/App.tsx
+++ marion/website/src/App.tsx
@@ -49,21 +49,27 @@ function goHome()
 	}
 }
 
+function authentification42()
+{
+	//todo
+}
+
 function LoginView()
 {
-	return (
-		<>
-			<h1><strong>Login to play</strong></h1>
+    return (
+        <>
+            <h1><strong>Login to play</strong></h1>
 
-			<div>
-				<input type="text" placeholder="Username" />
-				<input type="password" placeholder="Password" />
-				<button type="submit" onClick={goHome()}>connect</button>
-
-
-			</div>
-		</>
-	)
+            <div>
+                <input type="text" placeholder="Username" />
+                <input type="password" placeholder="Password" />
+				
+                <button type="submit" onClick={goHome()}>connect</button>
+            </div>
+            
+            <img onClick={authentification42} src="https://videos.openai.com/vg-assets/assets%2Ftask_01k1em5hpxezbagnsaf8yqxjxr%2F1753911434_img_1.webp?st=2025-07-30T20%3A17%3A52Z&se=2025-08-05T21%3A17%3A52Z&sks=b&skt=2025-07-30T20%3A17%3A52Z&ske=2025-08-05T21%3A17%3A52Z&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skoid=8ebb0df1-a278-4e2e-9c20-f2d373479b3a&skv=2019-02-02&sv=2018-11-09&sr=b&sp=r&spr=https%2Chttp&sig=iOKszktU%2FX12O4nJ9Bi%2FtelsvXyVKshI%2FXpTS4%2BcDWY%3D&az=oaivgprodscus" alt="42 logo" className="logo" />
+        </>
+    )
 }
 
 
