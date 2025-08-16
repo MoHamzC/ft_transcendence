08b41089bb3b2baabe64a49709313285091cdce0
marionelstrt <mariontarento@gmail.com>
2025-08-16 19:30:35 +0200

    feat : logo, fix : no longer backhome and menu buttons showing in game

--- mergetr/frontend/src/AboutUs.tsx
+++ mergetr/frontend/src/AboutUs.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import './AboutUs.css'; 
 import SplashCursor from './SplashCursor'
 
 
 export default function AboutUs() 
 {
+	const location = useLocation();
+	const hideAboutUs = location.pathname.startsWith('/pong/play');
+	if( hideAboutUs) return null;
 	const navigate = useNavigate();
 	return (
 		<>
