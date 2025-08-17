e029deb51c87ca76f988114735dd9adf5f6db280
Lucas Aubry | Mac <aubrylucas.pro@gmail.com>
2025-08-18 00:07:31 +0200

    +| ia (export)

--- mergetr/frontend/src/games/pong3d/script/Global.gd
+++ mergetr/frontend/src/games/pong3d/script/Global.gd
@@ -4,4 +4,13 @@ var score_left: int = 0
 var score_right: int = 0
 var max_score: int = 5
 var game_start: bool = false
-var right_ia: = false
+var right_ia: bool = false
+
+func _ready():
+	if Engine.has_singleton("JavaScriptBridge"):
+		var js = JavaScriptBridge.get_interface("window")
+		if js:  # Vérifie juste que l’objet existe
+			var search = str(js.location.search)  # "?ia=true" ou "?ia=false"
+			if search.find("ia=true") != -1:
+				right_ia = true
+	print("Right IA activé :", right_ia)
