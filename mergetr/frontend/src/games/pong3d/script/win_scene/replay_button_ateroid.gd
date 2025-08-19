1a56227fdac10355f302f3a2011743e7d5fe29b7
Lucas Aubry | Mac <aubrylucas.pro@gmail.com>
2025-08-19 15:52:03 +0200

    +| replay score max

--- mergetr/frontend/src/games/pong3d/script/win_scene/replay_button_ateroid.gd
+++ mergetr/frontend/src/games/pong3d/script/win_scene/replay_button_ateroid.gd
@@ -4,6 +4,6 @@ func _input_event(_camera, event, click_position, click_normal, shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		Global.score_left = 0
 		Global.score_right = 0
-		Global.max_score = 1
+		Global.max_score = 5
 		Global.game_start = false
 		get_tree().change_scene_to_file("res://scene/world.tscn")
