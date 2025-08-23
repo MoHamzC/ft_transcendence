97b4a4bec466fcb714a6b4a52677e6351dbee6fd
marionelstrt <mariontarento@gmail.com>
2025-08-23 18:25:57 +0200

    settings button

--- mergetr/frontend/src/Settings.tsx
+++ mergetr/frontend/src/Settings.tsx
@@ -1,10 +1,29 @@
 import FuzzyText from "./FuzzyText";
-
+import MyToggle from "./MyToggle";
 export default function Settings () {
   return (
 	<div className="settings">
 	  <FuzzyText>Settings</FuzzyText>
-	  <p>Coming soon...</p>
+	  <div>
+		<div className="setting-item flex flex-direction-column gap-4">
+		<h2> Double Auth</h2>
+		 <MyToggle 
+            onChange={(checked) => console.log('Toggle: Double Auth', checked)}
+            defaultChecked={false}
+        />
+		<h2> Private Profile</h2>
+		<MyToggle 
+            onChange={(checked) => console.log('Toggle: Private Profile', checked)}
+            defaultChecked={false}
+        />
+		<h2> Friends request</h2>
+		<MyToggle 
+            onChange={(checked) => console.log('Toggle: Private Profile', checked)}
+            defaultChecked={false}
+        />
+		
+				</div>
+	  </div>
 	</div>
   );
 }
