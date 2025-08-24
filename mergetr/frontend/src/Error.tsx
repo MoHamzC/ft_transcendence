b434f91a8b132283afead413bf047cbc6985a698
marionelstrt <mariontarento@gmail.com>
2025-08-24 23:04:12 +0200

    feat :error page

--- mergetr/frontend/src/Error.tsx
+++ mergetr/frontend/src/Error.tsx
@@ -1,20 +1,14 @@
+import './Error.css';
 import FuzzyText from "./FuzzyText";
 
 export default function Error() {
     return (
-        <div
-            style={{
-                color: "white",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "100vh",
-                textAlign: "center",
-            }}
-        >
-            <h1>
-                <FuzzyText>404 - Error</FuzzyText>
-            </h1>
+        <div className="error-overlay">
+            <div className="error-content">
+                <h1>
+                    <FuzzyText>404 - Error</FuzzyText>
+                </h1>
+            </div>
         </div>
     );
 }
