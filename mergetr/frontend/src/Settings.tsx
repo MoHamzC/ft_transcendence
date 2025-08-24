dbbf042eec9a591a4cbfa153aa58b39797ed3082
marionelstrt <mariontarento@gmail.com>
2025-08-24 17:41:06 +0200

    feat : starting to implement reset password function

--- mergetr/frontend/src/Settings.tsx
+++ mergetr/frontend/src/Settings.tsx
@@ -1,6 +1,14 @@
 import FuzzyText from "./FuzzyText";
 import MyToggle from "./MyToggle";
+import React from "react";
+import {useNavigate } from 'react-router-dom';
+
 export default function Settings () {
+	const navigate = useNavigate();
+
+	function goReset() {
+		navigate('/ResetPassword');
+	}
   return (
 	<div className="settings">
 	  <FuzzyText>Settings</FuzzyText>
@@ -21,6 +29,12 @@ export default function Settings () {
             onChange={(checked) => console.log('Toggle: Private Profile', checked)}
             defaultChecked={false}
         />
+		 <button
+                    className="px-4 py-2 rounded-full text-white mx-2 hover:cursor-pointer hover:bg-gray-500 active:scale-95 shadow-xl cursor-target"
+                    style={{ backgroundColor: 'oklch(25.7% 0.09 281.288)' }}
+                    type="submit"
+                    onClick={goReset}
+                >Reset Password </button>
 		
 				</div>
 	  </div>
