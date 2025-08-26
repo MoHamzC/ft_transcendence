6d0398be33ae6011f0339263762f6f1cc495f9a1
marionelstrt <mariontarento@gmail.com>
2025-08-27 01:13:01 +0200

    feat : select language button

--- mergetr/frontend/src/Settings.tsx
+++ mergetr/frontend/src/Settings.tsx
@@ -3,9 +3,11 @@ import MyToggle from "./MyToggle";
 import React from "react";
 import {useNavigate } from 'react-router-dom';
 import './Settings.css';
-
+import Select from "./Select"
 export default function Settings () {
 	const navigate = useNavigate();
+
+	const [language, setLanguage] = React.useState('en');
 
 	function goReset() {
 		navigate('/ResetPassword');
@@ -30,6 +32,8 @@ export default function Settings () {
             onChange={(checked) => console.log('Toggle: Private Profile', checked)}
             defaultChecked={false}
         />
+					<h2>Language</h2>
+						<Select value={language} onChange={(v) => setLanguage(v)} />
 		 <button
                     className="px-4 py-2 rounded-full text-white mx-2 hover:cursor-pointer hover:bg-gray-500 active:scale-95 shadow-xl cursor-target"
                     style={{ backgroundColor: 'oklch(25.7% 0.09 281.288)' }}
