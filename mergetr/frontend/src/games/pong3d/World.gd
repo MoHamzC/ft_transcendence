91de74b4538301eaba1503570742288779849f0a
Lucas Aubry | Mac <aubrylucas.pro@gmail.com>
2025-08-31 15:01:26 +0200

    ~| data api/match update

--- mergetr/frontend/src/games/pong3d/World.gd
+++ mergetr/frontend/src/games/pong3d/World.gd
@@ -5,22 +5,23 @@ func on_goal_scored():
 		await get_tree().create_timer(6.1).timeout
 		get_tree().change_scene_to_file("res://scene/victory_scene.tscn")
 		if Global.score_left > Global.score_right:
-			send_match_result("PlayerLeft", Global.score_left, Global.score_right)
+			send_match_result("PlayerLeft", "PlayerRight", Global.score_left, Global.score_right)
 		else:
-			send_match_result("PlayerRight", Global.score_left, Global.score_right)
+			send_match_result("PlayerRight", "PlayerLeft", Global.score_right, Global.score_left)
 
 #envoyer le score au back
 
 #reference au noeud http request
 @onready var http_request: HTTPRequest = $HTTPRequest
 
-func send_match_result(winner: String, score_p1: int, score_p2: int) -> void:
+func send_match_result(winner: String, looser: String, score_winner: int, score_looser: int) -> void:
 	var url = "http://localhost:5001/api/match"  # ton endpoint backend
 
 	var data := {
-		"winner": winner,
-		"player_left": score_p1,
-		"player_right": score_p2
+		"playerWinner": winner, #(le pseudo du perdant) 
+		"playerLooser": looser, #(le pseudo du looser)
+		"playerWinnerScore": score_winner,
+		"playerLooserScore": score_looser
 	}
 
 	#convertion en string json
